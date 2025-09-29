15c1cfe7ddc1b5b0793e274eb30ad612adb083a8
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-29 21:03:30 +0800

    added the js file and logic

--- Weather App/weather.js
+++ Weather App/weather.js
@@ -1,3 +1,26 @@
 let cityInput = document.getElementById('cityInput');
 let searchBtn = document.getElementById('searchButton');
-let display = document.getElementById('weatherDisplay');
+let weatherDisplay = document.getElementById('weatherDisplay');
+let weatherForm = document.getElementById('weatherForm');
+const APIkeys = "18b91061574008796645bad5f9ec0ecb";
+
+
+weatherForm.addEventListener('submit', async event => {
+    event.preventDefault();
+
+
+
+})
+
+async function getWeather(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkeys}`;
+
+    const response = await fetch(url);
+    console.log(response);
+
+    if(!response){
+        throw new Error("Cant fetch data!");
+    }
+
+    return response.json();
+}
