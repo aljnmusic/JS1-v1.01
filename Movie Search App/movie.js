dc6f99c33811959c3bdc26049f1aa86bbe539593
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-02 22:16:10 +0800

    created a new movie search app project

--- Movie Search App/movie.js
+++ Movie Search App/movie.js
@@ -3,13 +3,33 @@ let inputEl = document.getElementById('movieInput');
 let displayEl = document.getElementById('movieDisplay');
 const APIkeys = "8dccd3adfe5180eaeb6ee68440f24b02";
 
+
+
+formEl.addEventListener("submit", async event  => {
+    event.preventDefault();
+
+    const movie = inputEl.value;
+
+    if(movie){
+        try{
+            const movieData = await getMovie(movie);
+            console.log("Submitted");
+        } catch(e){
+            console.error(e);
+
+        }
+    } else {
+        throw new Error("Error encountered");
+    }
+})
+
 async function getMovie(movie){
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${APIkey}&query=${movieName}`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${APIkeys}&query=${movie}`;
 
     const response = await fetch(url);
     console.log(response);
 
-    if(!response){
+    if(!response.ok){
         throw new Error("Cant fetch data")
     }
 
