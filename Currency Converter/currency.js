d48d43701e8f5ca787b1a89a709310f0826f6d73
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-06 21:55:41 +0800

    created a new currency converter app project

--- Currency Converter/currency.js
+++ Currency Converter/currency.js
@@ -1,35 +1,49 @@
-let formEl = document.getElementById('currForm')
-let inputEl = document.getElementById('currInput')
-let convertBtn = document.getElementById('convertButton')
-let clearBtn = document.getElementById('clearButton')
-let dropdownEl =  document.getElementById('dropdownContent')
+    let formEl = document.getElementById('currForm')
+    let inputEl = document.getElementById('currInput')
+    let convertBtn = document.getElementById('convertButton')
+    let clearBtn = document.getElementById('clearButton')
+    let dropdownEl =  document.getElementById('dropdownContent')
+    let selected = document.getElementById('selected')
 
 
-formEl.addEventListener('submit', (event)=>{
-    event.preventDefault()
+    formEl.addEventListener('submit', (event)=>{
+        event.preventDefault()
 
 
 
-})
-
-dropdownEl.addEventListener('click', (event)=>{
-    event.preventDefault()
-
-    let list = document.getElementById('dropdownList')
-
-    if(list.classList.contains('hidden')){
-        list.classList.remove('hidden')
-    } else {
-        list.classList.add('hidden')
-    }
-
-    let currencyItems = document.querySelectorAll('#currencies li')
-
-    currencyItems.forEach(item => {
-        let selectedItem = item.textContent
-        let selected = document.getElementById('selected')
-
-        selected.textContent = selectedItem
     })
 
-})
+    dropdownEl.addEventListener('click', (event)=>{
+        event.preventDefault()
+
+        let list = document.getElementById('dropdownList')
+
+        if(list.classList.contains('hidden')){
+            list.classList.remove('hidden')
+        } else {
+            list.classList.add('hidden')
+        }
+
+        let currencies = document.querySelectorAll('#currencies li')
+        currencies.forEach(item => {
+            item.addEventListener('click', (event)=>{
+                event.preventDefault()
+
+                selected.textContent = item.textContent
+            })
+        })
+
+
+        // let currencyItems = document.querySelectorAll('#currencies li')
+        //
+        // currencyItems.forEach(item => {
+        //     let selectedItem = item.textContent
+        //
+        //     selected.textContent = selectedItem
+        //
+        //     inputEl.textContent = `Enter amount in ${selectedItem}`
+        //
+        //     dropdownEl.classList.remove('hidden')
+        // })
+
+    })
