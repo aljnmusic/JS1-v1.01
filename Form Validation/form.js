44805256a67b85469a53ffcd10c9a8f1ca81bbd1
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-07 22:04:42 +0800

    created a new form validation app

---
+++ Form Validation/form.js
@@ -0,0 +1,5 @@
+let formEl = document.getElementById('form-element');
+let submitBtn = document.getElementById('submitButton');
+let nameInput = document.getElementById('name');
+let emailInput = document.getElementById('email');
+let passwordInput = document.getElementById('password');
