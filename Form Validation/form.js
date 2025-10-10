d7e27d61ad42aaaf424d024eef1738e34686a590
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-10 22:46:33 +0800

    created a new form validation app

--- Form Validation/form.js
+++ Form Validation/form.js
@@ -5,20 +5,20 @@ let emailInput = document.getElementById('email');
 let passwordInput = document.getElementById('password');
 
 function validateName(){
-    let errorMessage = document.getElementById('error');
+    let errorMessage = document.getElementById('errorName');
 
-    if(nameInput === ''){
+    if(nameInput.value === ''){
         errorMessage.textContent = 'Name is required';
     }
 }
 
 function validateEmail(){
     let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
-    let errorMessage = document.getElementById('error');
+    let errorMessage = document.getElementById('errorEmail');
 
-    if(emailInput === ''){
+    if(emailInput.value === ''){
         errorMessage.textContent = 'Email is required';
-    } else if(emailInput !== emailPattern){
+    } else if(!emailPattern.test(emailInput.value)){
         errorMessage.textContent = 'Invalid email address';
     }
 }
@@ -27,11 +27,11 @@ function validatePassword(){
     let pass = '1234'
     let errorMessage = document.getElementById('error');
 
-    if(passwordInput === ''){
+    if(passwordInput.value === ''){
         errorMessage.textContent = 'Password is required';
     }
 
-    if(passwordInput !== pass){
+    if(!pass.test(passwordInput.value)){
         errorMessage.textContent = 'Invalid password';
     }
 }
