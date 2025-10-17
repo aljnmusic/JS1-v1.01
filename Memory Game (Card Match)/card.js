ff241b666125f8dda69b1ee8935f9885cbe5bdef
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-17 22:21:51 +0800

    created a new memory card game

---
+++ Memory Game (Card Match)/card.js
@@ -0,0 +1,11 @@
+let gridEl = document.getElementById('grid');
+let resetBtn = document.getElementById('reset');
+let playBtn = document.getElementById('play');
+
+let cardsData = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+let cards = []
+let flippedCards = [];
+let matchedCards = 0
+let lockBoard = false
+
