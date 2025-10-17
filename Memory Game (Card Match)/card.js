e381cb4ac72b9c0570d9e95412b9bdb0cf7449ab
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-17 22:41:42 +0800

    created a new memory card game

--- Memory Game (Card Match)/card.js
+++ Memory Game (Card Match)/card.js
@@ -9,3 +9,55 @@ let flippedCards = [];
 let matchedCards = 0
 let lockBoard = false
 
+function shuffleCards(array){
+    return array.sort(() => Math.random() - 0.5)
+}
+
+function createCards(){
+    cards = shuffleCards([...cardsData, ...cardsData])
+    gridEl.innerHTML = '';
+
+    cards.forEach(value => {
+        const card = document.createElement('div');
+        card.className = 'bg-gray-200 h-24 flex items-center justify-center cursor-pointer rounded shadow text-3xl font-bold select-none';
+        card.textContent = '?'
+        card.dataset.value = value;
+        card.addEventListener('click', () => flipCard(card))
+        gridEl.appendChild(card);
+    })
+
+    flippedCards = [];
+    matchedCards = 0;
+    lockBoard = false;
+}
+
+function flipCard(card){
+    if(lockBoard){
+        return;
+    }
+
+    if(flippedCards.includes(card)){
+        return;
+    }
+
+    if(card.textContent !== '?'){
+        return;
+    }
+
+    card.textContent = card.dataset.value;
+    flippedCards.push(card);
+
+    if(flippedCards.length === 2){
+        checkMatch()
+    }
+
+
+}
+
+
+function checkMatch(){
+
+}
+
+
+createCards()
