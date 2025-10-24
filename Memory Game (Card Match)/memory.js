55c1a5466eb6c0c69eb97f796f326847b3de9284
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-24 22:08:41 +0800

    created a createCard function

--- Memory Game (Card Match)/memory.js
+++ Memory Game (Card Match)/memory.js
@@ -12,3 +12,21 @@ let lockBoard = false
 function shuffleCards(array){
     return array.sort(() => Math.random() - 0.5)
 }
+
+function createCards(){
+    cards = shuffleCards([...cardsData, ...cardsData])
+    gridEl.innerHTML = ''
+
+    cards.forEach(card => {
+        const card = document.createElement('div')
+        card.className = 'bg-gray-200 h-24 flex items-center justify-center cursor-pointer rounded shadow select-none'
+        card.textContent = '?'
+        card.dataset.value = value
+        card.addEventListener('click', flipCard())
+        gridEl.appendChild(card)
+    })
+
+    flippedCards = []
+    matchedCards = 0
+    lockBoard = false
+}
