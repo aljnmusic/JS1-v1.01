fab7ade714aa8b356687de6288cb4a1a3184079d
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-24 21:57:19 +0800

    remove the old card.js file and created a new one for practice, initialize all the variables.

--- Memory Game (Card Match)/card.js
+++ Memory Game (Card Match)/card.js
@@ -1,86 +1,10 @@
-let gridEl = document.getElementById('grid');
-let resetBtn = document.getElementById('reset');
-let playBtn = document.getElementById('play');
 
-let cardsData = ['A', 'B', 'C', 'D', 'E', 'F'];
 
-let cards = []
-let flippedCards = [];
-let matchedCards = 0
-let lockBoard = false
+let cardsData = ['images/5.jpg', 'images/ad.jpg', 'images/f1.png', 'images/f4.png', 'images/kel.jpg', 'images/we.jpg'];
 
-function shuffleCards(array){
-    return array.sort(() => Math.random() - 0.5)
-}
+        card.className = 'bg-gray-200 h-24 flex items-center justify-center cursor-pointer rounded shadow select-none';
 
-function createCards(){
-    cards = shuffleCards([...cardsData, ...cardsData])
-    gridEl.innerHTML = '';
 
-    cards.forEach(value => {
-        const card = document.createElement('div');
-        card.className = 'bg-gray-200 h-24 flex items-center justify-center cursor-pointer rounded shadow text-3xl font-bold select-none';
-        card.textContent = '?'
-        card.dataset.value = value;
-        card.addEventListener('click', () => flipCard(card))
-        gridEl.appendChild(card);
-    })
-
-    flippedCards = [];
-    matchedCards = 0;
-    lockBoard = false;
-}
-
-function flipCard(card){
-    if(lockBoard){
-        return;
-    }
-
-    if(flippedCards.includes(card)){
-        return;
-    }
-
-    if(card.textContent !== '?'){
-        return;
-    }
-
-    card.textContent = card.dataset.value;
-    flippedCards.push(card);
-
-    if(flippedCards.length === 2){
-        checkMatch()
-    }
-
+        card.innerHTML = `<img src=${value} class="h-full w-full object-cover rounded" alt="card images" />`
 
 }
-
-
-function checkMatch(){
-    const [card1, card2] = flippedCards;
-
-    if(card1.dataset.value === card2.dataset.value){
-        matchedCards += 2;
-        flippedCards = []
-
-        if(matchedCards === cards.length){
-            setTimeout(() => alert('You won!'), 500)
-        }
-    } else {
-        lockBoard = true
-        setTimeout(() => {
-            flippedCards.forEach(card => (card.textContent = '?'))
-            flippedCards = []
-            lockBoard = false
-        }, 1000)
-    }
-
-}
-
-playBtn.addEventListener('click', createCards)
-resetBtn.addEventListener('click', () => {
-    createCards()
-    alert('Game Reset')
-})
-
-
-createCards()
---
+++ Memory Game (Card Match)/memory.js
@@ -0,0 +1,10 @@
+let gridEl = document.getElementById('grid')
+let playEl =  document.getElementById('play')
+let resetBtn = document.getElementById('reset')
+
+let cardsData = ['images/5.jpg', 'images/ad.jpg', 'images/f1.png', 'images/f4.png', 'images/kel.jpg', 'images/we.jpg'];
+
+let cards = []
+let flippedCards = []
+let matchedCards = 0
+let lockBoard = false
