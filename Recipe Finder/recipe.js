8e7bdbe386663ef69002b0f086952d0000281f98
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-31 22:42:40 +0800

    created a function to fetch recipe from an API via user input

--- Recipe Finder/recipe.js
+++ Recipe Finder/recipe.js
@@ -3,3 +3,24 @@ let searchBtn = document.getElementById('search')
 let clearBtn = document.getElementById('clear')
 let mainGrid = document.getElementById('main')
 let displayEl = document.getElementById('recipesDisplay')
+
+async function fetchRecipes() {
+    const query = recipe.value.trim()
+
+    if(!query) {
+        console.log("Enter a recipe name.")
+        return
+    }
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${encodeURIComponent(query)}`
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+searchBtn.addEventListener('click', fetchRecipes);
